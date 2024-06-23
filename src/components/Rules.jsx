d843f88d93dc83e417dbc7eadff2416ab3260aae
Douglas-Zuqueto <0001039234@senaimgaluno.com.br>
2024-06-23 16:42:46 -0300

    Criação e modificação da páginas de Regras do evento.

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -1,33 +1,105 @@
-import first from '../assets/images/colab1.jpg';
-
 
 export default function Rules() {
     return (
-        <div id="team"><br />
-            <h1>Melhor Equipe</h1>
-            <div className="teamtext">
-                <br />
-                <h4 className='flint'>Colaborador1</h4>
-                <img src={first} alt="pic" />
-                <br />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio iusto cumque commodi explicabo blanditiis alias, placeat sed, qui, at esse? Unde voluptate, eaque reiciendis numquam repellat suscipit deleniti voluptatum?</p>
-            </div>
+        <div id="rules"><br />
+            <h1 className="title">Regras Gerais do Evento</h1>
+            <p>Crianças até 7 anos de idade não pagam!</p>
 
-            <div className="teamtext">
-                <br />
-                <h4>Colaborador2</h4>
-                <img src={first} alt="pic" />
-                <br />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio iusto cumque commodi explicabo blanditiis alias, placeat sed, qui, at esse? Unde voluptate, eaque reiciendis numquam repellat suscipit deleniti voluptatum?</p>
-            </div>
+            <ol>
+                <li>Não será permitido entrar com armas de fogo (inclusive para aqueles que tiverem porte da mesma), 
+                    o mesmo vale para artifícios de efeitos pirotécnicos;</li>
+                
+                <li>Não será permitido entrar com objetos cortantes ou perfurantes tais como tesouras, estiletes, 
+                    compassos, facas, espadas ou similares. Solicitamos aos visitantes que não tragam este tipo de 
+                    material ao evento. Caso o visitante insista, o material será retido e deve ser retirado somente 
+                    ao final do evento. Em caso de esquecimento, o objeto deverá ser retirado na sede da organização 
+                    no prazo de 5 dias úteis contados a partir do fim do evento, após este período, o mesmo será 
+                    descartado;</li>
 
-            <div className="teamtext">
-                <br />
-                <h4>Colaborador3</h4>
-                <img src={first} alt="pic" />
-                <br />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore distinctio iusto cumque commodi explicabo blanditiis alias, placeat sed, qui, at esse? Unde voluptate, eaque reiciendis numquam repellat suscipit deleniti voluptatum?</p>
-            </div>
+                <li>Não será permitido entrar com bebidas alcoólicas, nem latas e garrafas de vidro que possam 
+                    trazer risco à segurança no evento. Elas serão confiscadas e descartadas. Assim como também 
+                    é proibido consumi-las ou portá-las dentro do evento. Os visitantes encontrados com bebidas 
+                    alcóolicas serão convidados a se retirarem do evento, assim como os que estiverem com sinais 
+                    visíveis de embriaguez;</li>
+                
+                <li>Não é permitida a entrada e nem o uso de entorpecentes ou substâncias controladas ou ilegais.</li>
+
+                <li>Os visitantes estão proibidos de colar ou distribuir qualquer tipo de cartaz ou panfleto sem a 
+                    autorização dos organizadores do evento, assim como andar com placas ofensivas a pessoas ou 
+                    empresas. O mesmo vale para “plaquinhas” com desenhos obscenos ou dizeres obscenos, impróprios 
+                    e que constituem algum ato difamatório, calunioso, e que possa trazer prejuízo a alguém ou a 
+                    organização do evento.</li>
+
+                <li>Não será tolerada a permanência do visitante que iniciar ou participar de tumultos ou qualquer 
+                    tipo de briga dentro do espaço do evento;</li>
+
+                <li>É proibido fumar dentro de qualquer dependência fechada do evento. Para os fumantes será 
+                    disponibilizada uma área de fumantes.</li>
+
+                <li>É proibida a venda de quaisquer produtos não autorizados (ambulantes) nas dependências do 
+                    evento. Os produtos em questão serão retidos e devolvidos apenas ao final do evento;</li>
+
+                <li>Serão coibidas toda e qualquer cena e vestimenta ilícita ou imprópria para TODOS os 
+                    visitantes (maiores ou menores de idade). Em caso de vestimenta imprópria, a pessoa deverá 
+                    se vestir apropriadamente (não serão permitidas pessoas de toalhas, sungas, biquínis, roupas 
+                    íntimas, sem roupas ou similares). Em caso de reincidência, a pessoa em questão será convidada 
+                    a se retirar do evento;</li>
+
+                <li>É proibida a entrada de animais em geral, exceto cão-guia, com os devidos documentos para a 
+                    comprovação, devendo ser apresentada na portaria do evento;</li>
+
+                <li>É permitido entrar no evento com armas do tipo airsoft, com tanto que elas estejam SEM 
+                    munição e com a sinalização laranja na ponta, que aponta não ser um armamento real. 
+                    Caso seja comprovado o não cumprimento desses requisitos, tal arma será retida pela 
+                    organização do evento e só poderá ser retirada ao final, no momento que os mesmos 
+                    deixarem o evento, garantindo assim a segurança de todos. Para os COSPLAYERS que necessitem 
+                    da arma em sua apresentação, disponibilizamos guarda-volumes, frisando que apenas e tão 
+                    somente no momento da apresentação será permitido o porte de tais armas, sendo que nas 
+                    demais ocasiões (circulação pelo evento) os cosplayers deverão deixá-las na área reservada 
+                    para tanto (guarda volumes).</li>
+
+                <li>Todos devem portar um documento de identidade com foto;</li>
+
+                <li>É proibida a entrada de Vape, ou qualquer outro tipo de cigarro eletrônico, visto que o 
+                    mesmo é proibido no Brasil;</li>
+
+                <li>É veemente proibido pingentes e correntes PESADAS, que possam causar ou infringir dano a outro;</li>
+
+                <li>É proibido a entrada de pistolas de água e laser pointers de qualquer cor e/ou potência;</li>
+
+                <li>Compete ao próprio visitante ter especial atenção aos seus pertences, visto que o evento não 
+                    tem responsabilidade alguma por itens esquecidos, perdidos, furtados e roubados ou qualquer 
+                    outro meio em que venha a ter seu item extraviado, pois não há dever de guarda e vigilância 
+                    a ser imposto a organizadora do evento;</li>
+
+                <li>Fica proibido veemente, fogos de artificio, dispositivos explosivos, sinalizadores e 
+                    aparatos incendiários de qualquer espécie, substâncias venenosas e/ou tóxicas, incluindo 
+                    drogas ilegais.</li>
+            </ol>
+
+            <h1 className="title2">Regras Sobre o Uso de Réplicas de Armas Brancas</h1>
+
+            <ol>
+                <li>Visando a segurança de todos, fica terminantemente proibido nas dependências do evento o 
+                    porte de réplicas e armas de metal de qualquer tipo, especialmente contundentes, 
+                    perfurantes, cortantes, perfuro-cortantes, como tesoura, cortador de unhas, gilete e/ou 
+                    lâmina de barbear. Solicitamos ao público em geral que não tragam nenhuma arma e/ou réplica 
+                    de metal para o evento, vez que não será permitido a entrada das mesmas. Para os COSPLAYERS 
+                    que necessitem da arma de metal em sua apresentação, disponibilizamos guarda-volumes, frisando 
+                    que apenas e tão somente no momento da apresentação será permitido o porte de tais armas, 
+                    sendo que nas demais ocasiões (circulação pelo evento) os cosplayers deverão deixá-las na 
+                    área reservada para tanto (guarda volumes);</li>
+
+                    <li>As armas e/ou réplicas de metais serão devolvidas aos cosplayers apenas no momento que 
+                        os mesmos deixarem o evento, garantindo assim a segurança de todos. O visitante que portar 
+                        réplicas de armas brancas que se encaixam na descrição, terá que deixá-las na portaria e 
+                        retirar somente ao deixar o evento. Fica autorizada a entrada de réplicas feitas 
+                        artesanalmente em madeira ou qualquer material mais simples (isopor, papel, borracha, etc), 
+                        contanto que não possuam corte ou sejam perfurantes. A organização reserva-se o direito de 
+                        apreender a arma de qualquer material, caso seja constatado que a mesma oferece perigo ou 
+                        não esteja de acordo com as normas do evento, devolvendo ao proprietário apenas no momento 
+                        que o mesmo deixar o evento, zelando assim pela segurança de todos os frequentadores.</li>
+            </ol>
         </div>
     )
 }
