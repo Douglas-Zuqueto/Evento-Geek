1fa0f081032b5a305cb962b9a2de1ac412d6c03c
jsvivis <0001039168@senaimgaluno.com.br>
2024-06-23 23:05:13 -0300

    .

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,33 +1,26 @@
+
 // slide página principal
 export default function Slider() {
-  return (
-    <div id="slider">
-      <div id="slidertext">
-        <h3>Geek Tech está chegando!</h3>
-        <br />
-        <h1>Evento novo na área!!!</h1>
-        <br />
-        <p>
-          Um novo evento para todos os Nerds e Otakus se divertirem! <br />
-          Diferentes atrações para todos os gostos e a chance de conhecer
-          Guilherme Briggs, grande dublador brasileiro.
-        </p>
-        <br />
-      </div>
+    return (
+        <div id="slider">
 
-      <div id="slidervideo">
-        <iframe
-          width="560"
-          height="315"
-          src="https://animeuai.com.br/wp-content/uploads/2024/06/ANIME-UAI-TV.mp4"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; 
-                encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
-      </div>
-    </div>
-  );
+            <div id="slidertext">
+                <h3>Geek Tech está chegando!</h3>
+                <br />
+                <h1>Evento novo na área!!!</h1><br />
+                <p>Um novo evento para todos os Nerds e Otakus se divertirem!  <br />
+                    Diferentes atrações para todos os gostos e a chance de conhecer Guilherme Briggs, 
+                    grande dublador brasileiro.</p><br />
+                
+            </div>
+
+            <div id="slidervideo">
+                <iframe width="560" height="315" src="https://animeuai.com.br/wp-content/uploads/2024/06/ANIME-UAI-TV.mp4" 
+                title="YouTube video player" frameBorder="0" allow="accelerometer; clipboard-write; 
+                encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" 
+                allowFullScreen></iframe>
+            </div>
+            
+        </div>
+    )
 }
