3a82bbaf873784ca1a2e0ee3c691100d6510f209
Douglas-Zuqueto <0001039234@senaimgaluno.com.br>
2024-06-23 23:15:39 -0300

    Ajustes no Slider.

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,20 @@
-import videoSlider from '../assets/images/Geektech.mp4'
+import videoSlider from "../assets/images/Geektech.mp4";
 // slide página principal
 export default function Slider() {
-    return (
-        <div id="slider">
+  return (
+    <div id="slider">
+      <div id="slidertext">
+        <h3>Geek Tech está chegando!</h3>
+        <br />
+        <h1>Evento novo na área!!!</h1>
+        <br />
+        <p>
+          Um novo evento para todos os Nerds e Otakus se divertirem! <br />
+          Diferentes atrações para todos os gostos e a chance de conhecer
+          Guilherme Briggs, grande dublador brasileiro.
+        </p>
+        <br />
+      </div>
 
       <div id="slidervideo">
         <iframe
