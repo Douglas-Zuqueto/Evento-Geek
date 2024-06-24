240ee850fe3493dc4708dd6a0c81a4c0aff4c9d6
Douglas-Zuqueto <0001039234@senaimgaluno.com.br>
2024-06-23 22:44:11 -0300

    Ajeitado cartão da equipe e identado o código corretamente.

--- src/components/Salas.jsx
+++ src/components/Salas.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import Salas from './Salasconteudo';
-import { listaSalas } from '../assets/dataSalas.js';
-import video from '../assets/images/videoEvento (3).mp4';
+import React from "react";
+import Salas from "./Salasconteudo";
+import { listaSalas } from "../assets/dataSalas.js";
+import video from "../assets/images/videoEvento (3).mp4";
 const MontSalas = () => {
-    const Sala = listaSalas.map((salaCont) => (
-        <Salas 
-            key={salaCont.id} 
-            name={salaCont.name} 
-            url={salaCont.imageurl} 
-            price={salaCont.price} 
-            reward={salaCont.reward} 
-            desc={salaCont.desc} 
-            comp={salaCont.comp} 
-        />
-    ));
+  const Sala = listaSalas.map((salaCont) => (
+    <Salas
+      key={salaCont.id}
+      name={salaCont.name}
+      url={salaCont.imageurl}
+      price={salaCont.price}
+      reward={salaCont.reward}
+      desc={salaCont.desc}
+      comp={salaCont.comp}
+    />
+  ));
 
-    return (
-            <div id="price">
-                <h1>Procura Diversão?</h1>
-                <h4>Escolha uma e venha fazer parte desta experiência</h4>
-                {Sala}
-                <div id="videoE">
-                    <video autoPlay loop muted width={500} >
-                        <source src={video} type="video/mp4" />
-                        Seu navegador não suporta o elemento de vídeo.
-                    </video>
-                </div>
-            </div>
-    );
-}
+  return (
+    <div id="price">
+      <h1>Procura Diversão?</h1>
+      <h4>Escolha uma e venha fazer parte desta experiência</h4>
+      {Sala}
+      <div id="videoE">
+        <video autoPlay loop muted width={500}>
+          <source src={video} type="video/mp4" />
+          Seu navegador não suporta o elemento de vídeo.
+        </video>
+      </div>
+    </div>
+  );
+};
 
 export default MontSalas;
